5c2dfab49633c7e3d15d4b76103735522e18771e
Gautham BA <gautham.bangalore@gmail.com>
2017-03-08 22:16:11 +0530

    Fixed the build file.

--- transpiler/src/build.bat
+++ transpiler/src/build.bat
@@ -1,13 +1,2 @@
 @ECHO OFF
-flex ./lex/linq_lexer.l
-ren lex.yy.c linq_lexer.c
-flex ./lex/codegen.l
-ren lex.yy.c codegen.c
-python ./utils/name_mangler.py codegen.c 1000 10000
-bison -dy linq_parser.y
-ren y.tab.c linq_parser.c
-gcc linq_lexer.c linq_parser.c codegen.c
-a.exe > plain.js
-python ./utils/strip_newline.py plain.js
-js-beautify plain.js | node
-del *.c y.tab.h a.exe
+flex ./lex/linq_lexer.l && ren lex.yy.c linq_lexer.c && flex ./lex/codegen.l && ren lex.yy.c codegen.c && python ./utils/name_mangler.py codegen.c 1000 10000 && bison -dy linq_parser.y && ren y.tab.c linq_parser.c && gcc linq_lexer.c linq_parser.c codegen.c && a.exe > plain.js && python ./utils/strip_newline.py plain.js && js-beautify plain.js | node && del *.c y.tab.h a.exe
